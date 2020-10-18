0dffdb25bef863e9ca291e3d767bc6bd3c917f74
Roberto14 <robertojesus14@gmail.com>
2020-10-18 20:55:08 +0200

    Replaced 'Experienced with' by 'Skills'

---
+++ src/components/ProgressionBar.tsx
@@ -0,0 +1,47 @@
+/** @jsx jsx */
+// @ts-ignore
+import React from "react"
+import { Link, jsx } from "theme-ui"
+
+type ProgressionBarType = {
+    text: string,
+    percentage: number
+}
+
+const ProgressionBar = ({ text, percentage }: ProgressionBarType) => {
+
+    const progressStyles = {
+        float: 'left',
+        width: `${percentage}%`,
+        height: '100%',
+        fontSize: 1,
+        fontWeight: 'bold',
+        lineHeight: '26px',
+        color: '#fff',
+        paddingLeft: '10px',
+        backgroundColor: 'secondary',
+        boxShadow: 'inset 0 -1px 0 rgba(0,0,0,.15)',
+        transition: 'width .6s ease',
+    }
+
+    const wrapperStyles = {
+        height: '26px',
+        width: [ '100%', '100%', '60%'],
+        marginBottom: '20px',
+        overflow: 'hidden',
+        backgroundColor: '#f5f5f5',
+        borderRadius: '4px',
+        boxShadow: 'inset 0 1px 2px rgba(0,0,0,.1)',
+    }
+
+
+    return (
+        <div sx={wrapperStyles}>
+            <div role="progressbar" sx={progressStyles}>
+                {text}
+            </div>
+        </div>
+    )
+}
+
+export default ProgressionBar
